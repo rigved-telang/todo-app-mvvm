99e3396280667e11206d32e7e3aab66f7ca52ba3
Rigved Telang <rigvedtelang20@gmail.com>
2023-03-02 10:53:06 +0530

    Adding files via android studio

---
+++ app/src/main/java/com/example/todolist/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.example.todolist.di
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.todolist.data.TodoDatabase
+import com.example.todolist.data.TodoRepository
+import com.example.todolist.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application):TodoDatabase{
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository{
+        return TodoRepositoryImpl(db.dao);
+    }
+}
